b158169f650cb7f1019592afbf14f041c50f53cf
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 17:02:14 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// IF STATEMENT REVIEW
+// Day of the Week: Prompt the user to enter a day of the week (e.g.,  "Monday") "")
+
+let user = prompt("whats the day of the week")
+if (user === "Saturday" || user === "sunday" || user === "saturday"){
+console.log("it's the weeked")
+}else{
+  console.log("its a school day")
+}
+// Age Group Classifier: Prompt the user to enter their age and categorize them into different age groups (child, teen, adult, senior)
+let age = prompt("what is your age?")
+parseInt(age)
+if (age < 0){
+  console.log("thats not an age")
+}
+if (age <= 12){
+  console.log("Your a child")
+} else if (age <= 18){
+  console.log("you are a teen")
+} else{
+  console.log("you lived a good life")
+}
+
+
+
+// WHILE LOOPS
+// Print 1 to 5
+let i = 0
+  while(i <=5){
+  console.log(i)
+    i++
+}
+
+
+
+// Print from a certain number to 1
+let user1 = prompt("Enter a number")
+user1 = parseInt(user)
+whle (user1 > 0) 
+  console.log(user1)
+  user1--
+
+
+
+
+
+// Summing up to 5
+let sum = 0 
+let counter = 1 
+while (sum <= 5){
+counter + sum
+  counter++
+}
+console.log("sum of 1-100", sum)
+
+
+// Using prompt
+let mysterystudent = "Zyare"
+let user4 = prompt("Enter a student name")
+while(mysterystudent !== user1){
+  user4 = prompt("Try agan guess a diff student")
+}
+console.log("Slow down")
+
+
